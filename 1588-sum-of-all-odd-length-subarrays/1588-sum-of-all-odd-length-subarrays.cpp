8a01db2672d4ee929ca8c6dba23397c4a7e67ae5
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2022-04-23 00:04:48 +0530

    Time: 9 ms (27.16%), Space: 8.2 MB (97.53%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -5,10 +5,11 @@ public:
         for(int i=0;i<arr.size();i++)
         {
             int curr=0;
+            
             for(int j=i;j<arr.size();j++){
                 curr+=arr[j];
                 
-                if(i%2 == j%2)
+                if((i&1) == (j&1))
                     ans+=curr;
             }
         }
