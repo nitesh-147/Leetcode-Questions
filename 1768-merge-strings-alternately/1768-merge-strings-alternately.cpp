a8222048cef856811ba1e36c5aecc980766ff076
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2022-04-24 11:13:53 +0530

    Time: 5 ms (20.17%), Space: 6.3 MB (55.47%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-       string a="";
+       string ans;//declaring String ans to store the resulting answer
         int m=0,n=0;
         for(int i=0;i<word1.size()+word2.size();i++)
         {
-            if(m<word1.length()&&(i%2==0||n>=word2.length()))
-                a+=word1[m++]; 
+            if(m<word1.length()&&(i%2==0||n>=word2.length())){//if m is less than length of word1 and i is even indexed or word 2  is fully traversed.
+                ans+=word1[m++]; 
+            }
             else
-                a+=word2[n++];
+                ans+=word2[n++];
         }
-        return a;
+        return ans;
     }
 };
