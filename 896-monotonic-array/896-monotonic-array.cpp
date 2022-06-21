24292ccd407a483b0b6fcd7e81fab3f0f6f8709b
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2022-06-21 12:59:17 +0545

    Time: 193 ms (81.44%), Space: 96.5 MB (18.14%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -8,18 +8,9 @@ public:
 //         if(nums==temp||nums==temp2)
 //             return true;
         
-        for(int i=1;i<nums.size();i++){
-            if(nums[0]>nums[nums.size()-1])
-            {
-                if(nums[i]>nums[i-1])
-                    return false;
-            }
-            else
-            {
-                if(nums[i]<nums[i-1])
-                    return false;
-            }
-        }
+         if(is_sorted(nums.begin(),nums.end()) || is_sorted(nums.begin(),nums.end(),greater<int>())){
         return true;
+    }
+    return false;
     }
 };
