b8da78286859afee40db211620b7188d22c12d69
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2022-08-23 23:23:09 +0530

    Time: 471 ms (20.41%), Space: 122.8 MB (30.97%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,30 +10,47 @@
  */
 class Solution {
 public:
-    void iter(ListNode* head,vector<int> &v){
-         ListNode* temp=head;
-         while(temp!=NULL){
-            v.push_back(temp->val);
-            temp=temp->next;
+//     void iter(ListNode* head,vector<int> &v){
+//          ListNode* temp=head;
+//          while(temp!=NULL){
+//             v.push_back(temp->val);
+//             temp=temp->next;
             
-        }
-    }
+//         }
+//     }
     
-    void rev(ListNode* head,vector<int> &v){
+//     void rev(ListNode* head,vector<int> &v){
+//         if(head==NULL)
+//             return ;
+//          rev(head->next,v);
+//         v.push_back(head->val);
+//     }
+    ListNode* temp;
+    void helper(ListNode* head,bool *ans){
+        // ListNode* temp=head;
         if(head==NULL)
-            return ;
-         rev(head->next,v);
-        v.push_back(head->val);
+            return;
+        
+        helper(head->next,ans);
+        if(temp->val!=head->val){
+             
+            *ans=false;
+        }
+           temp=temp->next;
     }
-    
     bool isPalindrome(ListNode* head) {
         // ListNode* temp=head;
-        vector<int> v,r;
-        iter(head,v);
-        rev(head,r);
-        if(r==v)
-            return true;
-        else
-            return false;
+        // vector<int> v,r;
+        // iter(head,v);
+        // rev(head,r);
+        // if(r==v)
+        //     return true;
+        // else
+        //     return false;
+        temp=head;
+        bool ans=true;
+        helper(head,&ans);
+        
+        return ans;
     }
 };
