e34bc841c30b6cbbd0451db4768cc299eb9633da
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2022-08-25 22:06:12 +0530

    Time: 42 ms (23.70%), Space: 9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int> m,n;
-        for(auto i:ransomNote)
-            m[i]++;
+        unordered_map<char,int> n;
+        // for(auto i:ransomNote)
+        //     m[i]++;
         for(auto i:magazine)
             n[i]++;
         
         for(auto i:ransomNote){
-            if(m[i]>n[i])
+            if(n[i]==0)
                 return false;
+            
+            n[i]--;
         }
         return true;
     }
