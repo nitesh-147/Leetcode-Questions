c894b833eba5450edd7735089431b06ea72bd5ac
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2022-09-11 10:14:32 +0530

    Time: 3 ms (67.47%), Space: 6 MB (11.14%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        if(n==0) return 0;
+        
+        else{
+            int result =0;
+            for(int i=0;i<32;i++){
+                result<<=1;
+                if(n&1) result++;
+                
+                n>>=1;
+            }
+            return result;
+        }
+    }
+};
