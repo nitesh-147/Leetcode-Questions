957db4b3fc24fce399ad5642c603d45aa4392256
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2022-09-25 12:09:42 +0530

    Time: 7 ms (55.44%), Space: 6.6 MB (89.27%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-       
+       int loc,i,j,x;
+       vector<int> ans;
         
-        int loc=0;
-        
-        vector<int> ans;
-        
-        for(int i=0;i<s.size();i++){
-            
-            int loc=s.find_last_of(s[i]);
-            for(int j=i+1;j<loc;j++){
-                int x=s.find_last_of(s[j]);
+       for(i=0;i<s.size();i++){
+            loc=s.find_last_of(s[i]);
+            for(j=i+1;j<loc;j++){
+                x=s.find_last_of(s[j]);
                 loc=max(loc,x);
             }
             ans.push_back(loc-i+1);
