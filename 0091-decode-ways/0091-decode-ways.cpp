c2479ad390e9a4dcc45ebea1e33b6677f06b489a
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2023-02-27 20:16:19 +0530

    Time: 14 ms (5.36%), Space: 16 MB (5.09%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     
+    // int nWays(string s,int i,vector<int> &dp){
+    //     if(s[i]=='0')return 0;
+    //     else if(i>=s.size()-1) return 1;
+    //     else if(dp[i]!=-1) return dp[i];
+    //     else if(s[i]=='1'|| (s[i]=='2' && s[i+1]>='0' && s[i+1]<='6')) return dp[i]=(nWays(s,i+1,dp)+nWays(s,i+2,dp));
+    //     else return dp[i]=nWays(s,i+1,dp);
+    // }
+   
     int nWays(string s,int i,vector<int> &dp){
         if(s[i]=='0')return 0;
         else if(i>=s.size()-1) return 1;
