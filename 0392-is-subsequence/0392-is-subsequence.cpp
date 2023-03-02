defcd11993bcc3d1c4a9bbda534786796810c0f2
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2023-03-02 17:32:58 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (29.48%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int pos=0;
-        for(int i=0;i<s.size();i++){
-            if(t.find(s[i],pos)+1)
-                pos=t.find(s[i],pos)+1;
-            else
-                return false;
-                
-            }
-        
-        return true;
+        int i=0,j=0;
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j])i++;
+            j++;
+        }
+        return i==s.size();
     }
 };
