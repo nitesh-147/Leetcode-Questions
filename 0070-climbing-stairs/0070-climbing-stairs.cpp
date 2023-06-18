520b3afc7bfda791df026078e4e3629eb7879359
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2023-06-18 20:56:42 +0545

    Time: 0 ms (100.00%), Space: 6.2 MB (15.10%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
+    vector<int> dp;
     int climbStairs(int n) {
-        if(n==1||n==2||n==3)
-            return n;
-        else if(n==4)
-            return 5;
-        else if(n==5)
-            return 8;
-        else if(n==6)
-            return 13;
-        else if(n==7)
-            return 21;
-        
-        return climbStairs(n-1)+climbStairs(n-2);
+        dp.resize(n+1,-1);
+        return helper(n);
+    }
+    
+private:
+    int helper(int n){
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=helper(n-1)+helper(n-2);
     }
 };
