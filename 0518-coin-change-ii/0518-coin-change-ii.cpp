35b01f3d5fa729070546ed4468148ad21a73ba5d
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2023-06-28 02:54:02 +0530

    Time: 7 ms (97.75%), Space: 18.7 MB (28.30%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
         
-        for(int i=0;i<=amount;i++){
-            dp[0][i]= (i%coins[0]==0);
-        }
+        return helper(n-1,amount,coins,dp);
+    }
+    
+    int helper(int n,int amount,vector<int> &coins,vector<vector<int>>& dp){
+        if(amount==0) return 1;
+        if(n<0) return 0;
         
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                int notTake=dp[i-1][j];
-                int take=0;
-                if(coins[i]<=j) take=dp[i][j-coins[i]];
-                dp[i][j]=take+notTake;
-            }
+        if(dp[n][amount]!=-1) return dp[n][amount];
+        
+        int notPick=helper(n-1,amount,coins,dp);
+        int pick=0;
+        if(amount>=coins[n]){
+            pick=helper(n,amount-coins[n],coins,dp);
         }
-        return dp[n-1][amount];
+        return dp[n][amount]=pick+notPick;
     }
 };
