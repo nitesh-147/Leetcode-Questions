467b3b8f6482b474e3cefeb1074bd79444be0d31
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2023-06-29 23:55:05 +0530

    Time: 92 ms (37.19%), Space: 24.5 MB (25.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -16,7 +16,7 @@ public:
        (s1,s2,ind1-1,ind2,dp));
    
 }
-    int helper(int m,int n,string s,string t,vector<vector<int>> &dp){
+    int helper(int m,int n,string& s,string& t,vector<vector<int>>& dp){
         if(m<0 || n<0) return 0;
         
         if(dp[m][n]!=-1) return dp[m][n];
@@ -31,6 +31,6 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int m=text1.size(),n=text2.size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return lcsUtil(text1,text2,m-1,n-1,dp);
+        return helper(m-1,n-1,text1,text2,dp);
     }
 };
