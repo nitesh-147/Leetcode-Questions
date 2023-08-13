fdad434820d2479e44952abbfaf9b68411430b9b
Nitesh Kumar Mehta <96474065+nitesh-147@users.noreply.github.com>
2023-08-13 15:48:07 +0530

    Time: 1908 ms (9.70%), Space: 0B (100.00%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,7 +1,15 @@
 # Write your MySQL query statement below
-SELECT employee_id,name,
-(select count(*) from Employees where reports_to=e.employee_id) as reports_count,
-(select round(avg(age)) from Employees where reports_to=e.employee_id) as average_age
-from Employees e 
-where (select avg(age) from Employees where reports_to=e.employee_id) is not null
-order by employee_id;
+# SELECT employee_id,name,
+# (select count(*) from Employees where reports_to=e.employee_id) as reports_count,
+# (select round(avg(age)) from Employees where reports_to=e.employee_id) as average_age
+# from Employees e 
+# where (select avg(age) from Employees where reports_to=e.employee_id) is not null
+# order by employee_id;
+
+select e1.employee_id,e1.name,
+count(*) as reports_count,
+round(avg(e2.age)) as average_age
+from Employees e1 inner join Employees e2 on
+e1.employee_id=e2.reports_to
+group by e1.employee_id
+order by e1.employee_id;
